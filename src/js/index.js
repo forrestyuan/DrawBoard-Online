b98cafbbcc109d013e0f7ade16628b4d66c8cf67
forrestyuan <yfcfight@sina.com>
2019-05-14 11:22:12 +0800

    update the logic of syncronize drawing

--- src/js/index.js
+++ src/js/index.js
@@ -1,250 +1,159 @@
 require("../css/index.css");
-const T = new require('../utils/index');
+import T from '../utils/index.js';
+import Chat from './chat.js'
+import DrawBoard from './drawBoard.js'
 
-class DrawBoard{
-	//构造器
-	constructor(obj){
-		//画布对象和上下文
-		this.canvas = obj.canvas;
-		this.ctx = obj.ctx;
-		this.winW = T.getTargetWH()[0]; //屏幕宽
-		this.winH = T.getTargetWH()[1]; //屏幕高
-		this.canvasW = this.winW * 0.99; //画布高
-		this.canvasH = this.winH *0.8; //画布高
-		this.canvas.width = this.canvasW;
-		this.canvas.height = this.canvasH;
-		this.canvasPadding = 5; //画布padding，用于界定边框线
-		//画笔 画布相关数据
-		this.ctx.lineWidth = obj.penceilWeight ||  2;
-		this.ctx.strokeStyle = obj.penceilColor || "#222";
-		this.canvas.style.backgroundColor = obj.canvasColor ||  'none';
-		//绘制堆栈
-		this.drawHistoryStack = [];
-		this.scaleList = [1,1]; //第一个参数用于调整画布的绘制缩放布宽为0.1，第二个参数为缩放倍率dpr
-		//时间旅行步数
-		this.timeTravelStep = -1;
-		//设置画布宽高
-		this.updateParam();
-	}
-
-	//更新参数 画布边界值和画布横纵坐标
-	updateParam(){
-		//预设参数2
-		this.drawLayerLeft = this.canvas.offsetLeft; //画布横坐标
-		this.drawLayerTop = this.canvas.offsetTop; //画布纵坐标
-		this.cansLimitLt = this.canvasPadding; //左边界
-		this.cansLimitRt = this.canvasW - this.canvasPadding; //右边界
-		this.cansLimitTp = this.canvasPadding; //上边界
-		this.cansLimitBt = this.canvasH - this.canvasPadding; //下边界
-	}
-	//更新上下文样式参数
-	updateCtxStyle(obj){
-		this.ctx.lineWidth = obj.penceilWeight ||  this.ctx.lineWidth;
-		this.ctx.strokeStyle = obj.penceilColor || this.ctx.strokeStyle;
-		this.canvas.style.backgroundColor = obj.canvasColor ||  this.canvas.style.backgroundColor;
-	}
-	
-  /**
-   * @desc 返回鼠标在画布上的横纵坐标
-   * @param Object event 事件对象(可选)
-   * @return Array [x,y]
-   */
-  mouseXY(event) {
-    event = event || window.event;
-    let x =
-      event.clientX ||
-      event.pageX ||
-      event.touches[0].clientX ||
-      event.touches[0].pageX;
-    let y =
-      event.clientY ||
-      event.pageY ||
-      event.touches[0].clientY ||
-			event.touches[0].pageY;
-
-    return [
-			(x- this.drawLayerLeft) / this.scaleList[1],
-			(y- this.drawLayerTop) / this.scaleList[1]
-		];
-	}
-
-	//绘制堆栈进入操作
-	pushStack(){
-		if(this.drawHistoryStack.length > 19){
-			this.drawHistoryStack.shift();
-			this.drawHistoryStack.push(this.canvas.toDataURL());
-		}else{
-			this.timeTravelStep ++;
-			this.drawHistoryStack.push(this.canvas.toDataURL());
-		}
-	}
-
-	/**
-	 * @desc 同步数据方法(通过socket.io传送数据)
-	 * @param Array axis 坐标数组
-	 */
-	syncData(axis) {
-		console.log(axis);
-	};
-	/**
-	 * @desc 绘制事件绑定监听
-	 * @param Boolean isunbind 解除所有是与绘制相关的绑定事件
-	 */
-	drawEvent(isUnbind) {
-		let eventStart = void 0,
-			eventEnd = void 0,
-			eventMove = void 0;
-		if ("ontouchstart" in window) {
-			eventStart = "ontouchstart";
-			eventEnd = "ontouchend";
-			eventMove = "ontouchmove";
-		} else {
-			eventStart = "onmousedown";
-			eventEnd = "onmouseup";
-			eventMove = "onmousemove";
-		}
-		if (isUnbind) {
-			this.canvas[eventStart] = null;
-			this.canvas[eventMove] = null;
-			this.canvas[eventEnd] = null;
-			return void 0;
-		}
-		//监听开始触摸（点击）屏幕事件
-		this.canvas[eventStart] = e =>{
-			this.ctx.beginPath();
-			//监听开始滑动绘制事件
-			this.canvas[eventMove] = e =>{
-				let mouseAxis = this.mouseXY(e);
-				if (
-					mouseAxis[0] < this.cansLimitLt ||
-					mouseAxis[0] > this.cansLimitRt ||
-					mouseAxis[1] < this.cansLimitTp ||
-					mouseAxis[1] > this.cansLimitBt
-				) {
-					this.canvas[eventMove] = null;
-				} else {
-					this.ctx.lineTo(mouseAxis[0], mouseAxis[1]);
-				}
-				this.ctx.stroke();
-			};
-		};
-		//监听触摸（点击）屏幕事件结束，置空滑动事件和将当前画布信息进栈
-		this.canvas[eventEnd] = e => {
-			this.canvas[eventMove] = null;
-			this.pushStack();
-		};
-	};
-	//画布历史穿梭（前进和后退）
-	travel(dir){
-		if( this.drawHistoryStack.length > 0){
-			if(dir < 0){	
-				if(--this.timeTravelStep < -1 ){
-					this.timeTravelStep = -1;
-					return;
-				}
-			}else if(dir > 0){
-				if(++this.timeTravelStep >= this.drawHistoryStack.length ){
-					this.timeTravelStep = this.drawHistoryStack.length - 1; 
-					return;
-				}
-			}
-			let cmDrawImg = (cb)=>{
-				let img = new Image();
-				img.src = this.drawHistoryStack[this.timeTravelStep];
-				
-				img.onload = ()=>{
-					this.ctx.drawImage(img,0,0);
-				}
-			}
-			this.ctx.clearRect(0,0,this.canvasW,this.canvasH);
-			cmDrawImg();
-		}
-	}
-};
-
-/**
- * @desc 初始化方法
- */
-function init(db) {
-	db.drawEvent();
-  window.onresize = function() {
-		db.drawEvent(true);
-		db.updateParam();
-		db.drawEvent();
-		db.travel(0)
-  };
-}
-
-function scaleHandler(dprBox, obj, isLarge){
-	if(isLarge){
-		dprBox.value = (+dprBox.value + 0.1).toFixed(1);
-		obj.scaleList[1] = (+ dprBox.value);
-		if(obj.scaleList[1] > 5){
-			obj.scaleList[1]=5;
-			dprBox.value=5;
-			return;
-		}
-	}else{
-		dprBox.value = (+dprBox.value - 0.1).toFixed(1);
-		obj.scaleList[1] = (+ dprBox.value);
-		if(obj.scaleList[1] < 0.1){
-			obj.scaleList[1]= 0.1;
-			dprBox.value=0.1;
-			return;
-		}
-	}
-	obj.canvas.style.width = obj.canvasW * obj.scaleList[1]+'px';
-	obj.canvas.style.height = obj.canvasH * obj.scaleList[1]+'px';
-	obj.updateParam();
-	console.log(obj.drawLayerLeft)
-}
-
-window.onload = function() {
+const userInfo = {};
+window.onload = function () {
+	//其它事件
 	let showPanel = false;
 	//画布对象和上下文
-  let canvas = document.getElementById("canvas");
+	let canvas = T.getEle("#canvas");
 	let ctx = canvas.getContext("2d");
-	let db = new DrawBoard({ canvas: canvas, ctx: ctx , penceilWeight: penceilWeight});
-	init(db);
 
-	T.getEle("#backward").onclick = function(){
+	//协作设置
+	userInfo.username = prompt("what is your name:") || `用户${Math.random()*100}`;
+	sessionStorage.setItem("drawusername", userInfo.username);
+	T.getEle('.userNameTag').innerHTML = userInfo.username;
+
+	let msgBox = T.getEle('.msgBox');
+	let msgInput = T.getEle('.msgTxt');
+	let genChatTpl = (name = 'unkonw', content = 'unkonw') => {
+		return `
+			<div>
+				<span>${name}说:&nbsp;&nbsp;${content}</span><br>
+			</div> 
+			`
+	};
+	//实例化聊天对象
+	let chat = new Chat({
+		receive: [{
+				socketName: 'getChatData',
+				callback: res => {
+					if (res) {
+						try {
+							res = JSON.parse(res)
+							msgBox.innerHTML += genChatTpl(res.username, res.msg);
+						} catch (e) {
+							console.error('返回数据有误');
+						}
+					}
+				}
+			},
+			{
+				socketName: 'updateUserList',
+				callback: res => {
+					try {
+						res = JSON.parse(res);
+						T.getEle('.wrapUserList').innerHTML = '';
+						res.forEach(item => {
+							T.getEle('.wrapUserList').innerHTML += `<span>${item}</span>`;
+						})
+					} catch (error) {
+						console.error("数据格式出错");
+					}
+				}
+			},
+			{
+				socketName: 'resetBeginPath',
+				callback: res => {
+					try {
+						res = JSON.parse(res);
+						if (res.status) {
+							if (res.username != userInfo.username) {
+								ctx.beginPath();
+							}
+						}
+					} catch (error) {
+						console.log(error);
+					}
+				}
+			}
+		]
+	});
+	//实例化画板
+	const db = new DrawBoard({
+		canvas,
+		ctx,
+		penceilWeight,
+		winW: T.getTargetWH()[0],
+		winH: T.getTargetWH()[1]
+	}, chat.getSocket());
+	//配置重置
+	chat.getSocket().on('resetConfig', res=>{
+		try {
+		res = JSON.parse(res);
+			console.log(res)
+			if (res.username != userInfo.username) {
+				res.config.travel !=0 ? db.travel(res.config.travel) : false;
+				res.config.clearCanvas ? db.clearCanvas() : false;
+				res.config.penceilWeight ? db.updateCtxStyle({penceilWeight: res.config.penceilWeight}) : false;
+				res.config.penceilColor ? db.updateCtxStyle({penceilColor: res.config.penceilColor}) : false;
+				res.config.canvasColor ? db.updateCtxStyle({canvasColor:res.config.canvasColor}): false;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	});
+	//封装发送配置信息
+	
+	//相关事件监听//需要同步事件
+	T.getEle("#backward").onclick = () => {
 		db.travel(-1)
-	}
-	T.getEle("#forward").onclick = function(){
+		chat.sendData('syncConfig',JSON.stringify({username:userInfo.username, config:{travel:-1}}));
+	};
+	T.getEle("#forward").onclick = () => {
 		db.travel(1)
+		chat.sendData('syncConfig',JSON.stringify({username:userInfo.username, config:{travel:1}}));
+	};
+	
+	T.getEle("#clearAll").onclick = () => {
+		db.clearCanvas();
+		chat.sendData('syncConfig',JSON.stringify({username:userInfo.username, config:{clearCanvas:true}}));
 	}
-	T.getEle("#clearAll").onclick = function(){
-		db.ctx.clearRect(0,0,db.canvasW,db.canvasH);
-		db.timeTravelStep = -1;
-		db.drawHistoryStack = [];
+	T.getEle("#penceilWeight").onchange = function () {
+		this.value = this.value > 120 ? 120 : this.value;
+		this.value = this.value < 1 ? 1 : this.value;
+		db.updateCtxStyle({penceilWeight: this.value});
+		chat.sendData('syncConfig',JSON.stringify({username:userInfo.username, config:{penceilWeight:this.value}}));
 	}
-	T.getEle("#penceilWeight").onchange = function(){
-		if(this.value > 120){
-			this.value = 120;
-		}
-		if(this.value < 1){
-			this.value = 1;
-		}
-		db.updateCtxStyle({penceilWeight:this.value});
+	T.getEle("#penceilColor").onchange = function () {
+		db.updateCtxStyle({	penceilColor: this.value});
+		chat.sendData('syncConfig',JSON.stringify({username:userInfo.username, config:{penceilColor:this.value}}));
 	}
-	T.getEle("#penceilColor").onchange = function(){
-		db.updateCtxStyle({penceilColor:this.value});
+	T.getEle("#canvasColor").onchange = function () {
+		db.updateCtxStyle({canvasColor: this.value	});
+		chat.sendData('syncConfig',JSON.stringify({username:userInfo.username, config:{canvasColor:this.value}}));
 	}
-	T.getEle("#canvasColor").onchange = function(){
-		db.updateCtxStyle({canvasColor:this.value});
-	}
-
 	let scaleNum = T.getEle("#scaleNum");
-	T.getEle("#larger").onclick=function(){
-		scaleHandler(scaleNum,db,true);
-	}
-	T.getEle("#smaller").onclick = function(){
-		scaleHandler(scaleNum,db,false);
-	}
-	T.getEle('.shareLayerBtn').onclick = function(){
+	T.getEle("#larger").onclick = () => {
+		db.scaleHandler(scaleNum, true);
+	};
+	T.getEle("#smaller").onclick = () => {
+		db.scaleHandler(scaleNum, false);
+	};
+
+	//显示关闭消息面板
+	let panelShowHideHandler = () => {
 		showPanel = !showPanel;
-		T.getEle(".sub-window").style = showPanel?"display:inline-block":"display:none"
+		T.getEle(".sub-window").style = showPanel ? "display:inline-block" : "display:none";
 	}
+	T.getEle('.shareLayerBtn').onclick = (e) => panelShowHideHandler();
+	T.getEle(".close-panel").onclick = (e) => panelShowHideHandler();
 
-
+	//发送聊天消息
+	T.getEle('.sendBtn').onclick = function () {
+		chat.sendData('chatData', JSON.stringify({
+			"username": userInfo.username,
+			"msg": msgInput.value
+		}), res => {
+			if (res) {
+				msgInput.value = '';
+			} else {
+				alert("发送消息中断");
+			}
+		});
+	};
+	//添加用户
+	chat.sendData('addUser', userInfo.username)
 };
